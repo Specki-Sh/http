f57675682c88dc3473c1175d07ce90fe1a05e23c
sp-Shakzhod <yushakhzod@gmail.com>
2021-05-18 13:44:21 +0500

    22

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/Specki-Sh/http/pkg/server"
 )
 
+const CRLF = "\r\n"
+
 func main() {
 	host := "0.0.0.0"
 	port := "9999"
@@ -24,10 +26,10 @@ func execute(host string, port string) (err error) {
 		body := "Welcome to our web-site"
 
 		_, err = conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Content-Length: " + strconv.Itoa(len(body)) + "\r\n" +
-				"Content-Type: text/html\r\n" +
-				"Connection: close\r\n" +
+			"HTTP/1.1 200 OK" + CRLF +
+				"Content-Length: " + strconv.Itoa(len(body)) + CRLF +
+				"Content-Type: text/html" + CRLF +
+				"Connection: close" + CRLF +
 				"\r\n" +
 				body,
 		))
@@ -39,10 +41,10 @@ func execute(host string, port string) (err error) {
 		body := "About Golang Academy"
 
 		_, err = conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Content-Length: " + strconv.Itoa(len(body)) + "\r\n" +
-				"Content-Type: text/html\r\n" +
-				"Connection: close\r\n" +
+			"HTTP/1.1 200 OK" + CRLF +
+				"Content-Length: " + strconv.Itoa(len(body)) + CRLF +
+				"Content-Type: text/html" + CRLF +
+				"Connection: close" + CRLF +
 				"\r\n" +
 				body,
 		))
@@ -52,63 +54,3 @@ func execute(host string, port string) (err error) {
 	})
 	return srv.Start()
 }
-
-// 	listener, err := net.Listen("tcp", net.JoinHostPort(host, port))
-// 	if err != nil {
-// 		log.Print(err)
-// 		return err
-// 	}
-// 	defer func() {
-// 		if cerr := listener.Close(); cerr != nil {
-// 			if err == nil {
-// 				err = cerr
-// 				return
-// 			}
-// 			log.Print(cerr)
-// 		}
-// 	}()
-
-// 	for {
-// 		conn, err := listener.Accept()
-// 		if err != nil {
-// 			log.Print(err)
-// 			//Идём давать мёд следующему
-// 			continue
-// 		}
-
-// 		err = handle(conn)
-// 		if err != nil {
-// 			log.Print(err)
-// 			continue
-// 		}
-// 	}
-
-// 	//return
-// }
-
-// func handle(conn net.Conn) (err error) {
-// 	defer func() {
-// 		if cerr := conn.Close(); cerr != nil {
-// 			if err == nil {
-// 				err = cerr
-// 				return
-// 			}
-// 			log.Print(err)
-// 		}
-// 	}()
-// 	// TODO: handle connection
-// 	conn.Write([]byte("Hello!\r\n"))
-
-// 	buf := make([]byte, 4096)
-// 	for {
-// 		n, err := conn.Read(buf)
-// 		if err == io.EOF {
-// 			log.Printf("%s", buf[:n])
-// 			return nil
-// 		}
-// 		if err != nil {
-// 			return err
-// 		}
-// 		log.Printf("%s", buf[:n])
-// 	}
-// }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,100 +1,56 @@
-package server
+package main
 
 import (
-	"bytes"
 	"log"
 	"net"
-	"strings"
-	"sync"
+	"os"
+	"strconv"
+
+	"github.com/ripol92/http/pkg/server"
 )
 
-type HandlerFunc func(conn net.Conn)
+const CRLF = "\r\n"
 
-type Server struct {
-	addr     string
-	mu       sync.RWMutex
-	handlers map[string]HandlerFunc
-}
+func main() {
+	host := "0.0.0.0"
+	port := "9999"
 
-func NewServer(addr string) *Server {
-	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
-}
-
-func (s *Server) Register(path string, handler HandlerFunc) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.handlers[path] = handler
-}
-
-func (s *Server) Start() error {
-	listener, err := net.Listen("tcp", s.addr)
-	if err != nil {
-		log.Print(err)
-		return err
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
 	}
-	defer func() {
-		if cerr := listener.Close(); cerr != nil {
-			if err == nil {
-				err = cerr
-				return
-			}
-			log.Print(cerr)
-		}
-	}()
+}
 
-	for {
-		conn, err := listener.Accept()
+func execute(host string, port string) (err error) {
+	srv := server.NewServer(net.JoinHostPort(host, port))
+	srv.Register("/", func(conn net.Conn) {
+		body := "Welcome to our web-site"
+
+		_, err = conn.Write([]byte(
+			"HTTP/1.1 200 OK" + CRLF +
+				"Content-Length: " + strconv.Itoa(len(body)) + CRLF +
+				"Content-Type: text/html" + CRLF +
+				"Connection: close" + CRLF +
+				"\r\n" +
+				body,
+		))
 		if err != nil {
 			log.Print(err)
-			return err
-
 		}
+	})
+	srv.Register("/about", func(conn net.Conn) {
+		body := "About Golang Academy"
 
-		err = s.handle(conn)
+		_, err = conn.Write([]byte(
+			"HTTP/1.1 200 OK" + CRLF +
+				"Content-Length: " + strconv.Itoa(len(body)) + CRLF +
+				"Content-Type: text/html" + CRLF +
+				"Connection: close" + CRLF +
+				"\r\n" +
+				body,
+		))
 		if err != nil {
 			log.Print(err)
-			continue
 		}
-	}
-
-}
-
-func (s Server) handle(conn net.Conn) (err error) {
-	defer func() {
-		if cerr := conn.Close(); cerr != nil {
-			if err == nil {
-				err = cerr
-				return
-			}
-			log.Print(err)
-		}
-	}()
-
-	buf := make([]byte, 4096)
-
-	n, err := conn.Read(buf)
-	data := buf[:n]
-	requestedLineDelim := []byte{'\r', '\n'}
-	requestedLineEnd := bytes.Index(data, requestedLineDelim)
-	if requestedLineEnd == -1 {
-		return err
-	}
-
-	requestLine := string(data[:requestedLineEnd])
-	parts := strings.Split(requestLine, " ")
-	if len(parts) != 3 {
-		return err
-	}
-
-	_, path, version := parts[0], parts[1], parts[2]
-
-	if version != "HTTP/1.1" {
-		return
-	}
-
-	if handler, exist := s.handlers[path]; exist {
-		handler(conn)
-	}
-
-	return nil
+	})
+	return srv.Start()
 }
